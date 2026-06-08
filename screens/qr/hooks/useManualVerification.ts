353c2599a5b09d8cd98d9887edd9d91838ee09d3
poko233 <victorterrazasc05@gmail.com>
2026-06-08 13:53:17 -0400

    add text to speech

--- screens/qr/hooks/useManualVerification.ts
+++ screens/qr/hooks/useManualVerification.ts
@@ -1,6 +1,7 @@
 // screens/qr/hooks/useManualVerification.ts
 import { getLocationString } from "@/utils/getLocation";
 import { decryptQrData } from "@/utils/qrCrypto";
+import * as Speech from "expo-speech";
 import { useCallback, useState } from "react";
 import { QrService } from "../services/qr.service";
 import { InputMode, VerifyAccessResponse } from "../types/qr.types";
@@ -11,11 +12,13 @@ export function useManualVerification() {
   const [loading, setLoading] = useState(false);
 
   const verify = useCallback(async (mode: InputMode, value: string) => {
+    // Detener cualquier habla anterior antes de empezar
+    Speech.stop();
+
     setLoading(true);
     setError(null);
     setResult(null);
     try {
-      // Obtener ubicación (si falla, usará el fallback)
       const location = await getLocationString();
       console.log(`📍 Ubicación final: "${location}"`);
 
@@ -27,6 +30,14 @@ export function useManualVerification() {
       } else {
         data = await QrService.verifyAccessByCI(value, location);
       }
+
+      const texto = `${data.usuario.nombres}${data.usuario.apellido_paterno}, ${data.alerta.mensaje}`;
+      Speech.speak(texto, {
+        language: "es-AR",
+        pitch: 0.7,
+        rate: 1.3,
+      });
+
       setResult(data);
     } catch (err: any) {
       setError(err?.message || "Error al verificar");
@@ -36,6 +47,7 @@ export function useManualVerification() {
   }, []);
 
   const reset = useCallback(() => {
+    Speech.stop(); // Detener habla al cerrar el panel
     setResult(null);
     setError(null);
   }, []);
